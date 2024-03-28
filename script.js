ec338a7599515fd1496f9e3a40583799b6bccee6
Nodirjonov <sobirovakomila85@gmail.com>
2024-03-29 00:05:46 +0500

    5oy js3

---
+++ script.js
@@ -0,0 +1,23 @@
+// let myobject = {
+//     id:11,
+//     parol:123456,
+//     name:'Fayzullox',
+//     email:'@gmail.com',
+//     guruh:981,
+// }
+// delete myobject.id
+// console.log(myobject);
+// myobject.id=11
+// const person = {fname:"John", lname:"Doe", age:25};
+
+// let text = "";
+// for (let x in person) {
+//   text += person[x];
+// }
+// console.log(person);
+
+const MyObject = {name: "John", Age: 30, City: "New York"};
+
+for (let key in MyObject) {
+  console.log(MyObject[key]);
+}
